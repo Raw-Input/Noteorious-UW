61c8b24682b80034eb9c9238a5f91fafce489258
taylormf <taylormf@uw.edu>
2020-01-23 14:01:19 -0800

    Updated with rich text editor sample

--- Noteorious/MainWindow.xaml.cs
+++ Noteorious/MainWindow.xaml.cs
@@ -1,99 +1,73 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
 using System.Windows.Controls;
 
-namespace Noteorious
+namespace Noteorious.Rich_text_controls
 {
-    public partial class MainWindow : Window
-    {
-        Microsoft.Win32.OpenFileDialog mDlgOpen = new Microsoft.Win32.OpenFileDialog();
-        Microsoft.Win32.SaveFileDialog mDlgSave = new Microsoft.Win32.SaveFileDialog();
+	public partial class RichTextEditorSample : Window
+	{
+		public RichTextEditorSample()
+		{
+			InitializeComponent();
+			cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+			cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+		}
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            ResetDlgs();
-        }
+		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+			btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+			btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+			btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
 
-        private void UpdateStatBar(string message)
-        {
-            mStatText.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " + message;
-        }
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			cmbFontFamily.SelectedItem = temp;
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+			cmbFontSize.Text = temp.ToString();
+		}
 
-        private void ResetDlgs()
-        {
-            mDlgOpen.FileName = "";
-            mDlgSave.FileName = "";
-            UpdateStatBar("Ready");
-        }
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Load(fileStream, DataFormats.Rtf);
+			}
+		}
 
-        private void mBtnClear_Click(object sender, RoutedEventArgs e)
-        {
-            ResetDlgs();
-            mTB.Text = "";
-        }
+		private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			SaveFileDialog dlg = new SaveFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Save(fileStream, DataFormats.Rtf);
+			}
+		}
 
-        private void mBtnOpen_Click(object sender, RoutedEventArgs e)
-        {
-            if (mDlgOpen.ShowDialog() == true)
-            {
-                System.IO.StreamReader reader = new System.IO.StreamReader(mDlgOpen.FileName);
-                mTB.Text = reader.ReadToEnd();
-                reader.Close();
-                UpdateStatBar("Read " + mDlgOpen.FileName);
-            }
-        }
+		private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (cmbFontFamily.SelectedItem != null)
+				rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+		}
 
-        private void mCBSpellCheck_Toggle(object sender, RoutedEventArgs e)
-        {
-            if (mTB != null)
-            {
-                mTB.SpellCheck.IsEnabled = (mCBSpellCheck.IsChecked == true);
-                UpdateStatBar((mTB.SpellCheck.IsEnabled ? "En" : "Dis") + "abled spell check");
-            }
-        }
-
-        private void mCBReadOnly_Toggle(object sender, RoutedEventArgs e)
-        {
-            if (mTB != null)
-            {
-                mTB.IsReadOnly = (mCBReadOnly.IsChecked == true);
-                UpdateStatBar((mTB.IsReadOnly ? "En" : "Dis") + "abled read only");
-            }
-        }
-
-        private void mBtnSaveAs_Click(object sender, RoutedEventArgs e)
-        {
-            SaveFileAs();
-        }
-
-        private void mBtnSave_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.Compare(mDlgSave.FileName, "") == 0)
-                SaveFileAs();
-            else
-                SaveFile();
-        }
-
-        private void SaveFileAs()
-        {
-            if (mDlgSave.ShowDialog() == true)
-                SaveFile();
-            else
-                UpdateStatBar("Text not saved to file.");
-        }
-
-        private void SaveFile()
-        {
-            System.IO.StreamWriter writer = new System.IO.StreamWriter(mDlgSave.FileName);
-            writer.Write(mTB.Text);
-            writer.Close();
-            UpdateStatBar("Wrote " + mTB.Text.Length.ToString() + " chars in " + mDlgSave.FileName);
-        }
-
-        private void mTB_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-    }
+		private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+		}
+	}
 }
