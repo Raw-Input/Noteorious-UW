83d3cf37d99a43e3f9064c62c3e8c8be1ab75eff
taylormf <taylormf@uw.edu>
2020-01-23 17:13:16 -0800

    Preliminary work for tabbed interface

---
+++ Noteorious/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Noteorious
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel ()
+        {
+            NewTabCommand = new ActionCommand(p => NewTab());
+            Tabs = new ObservableCollection<ITab>();
+        }
+        
+        public ICommand NewTabCommand { get; }
+        public ICollection<ITab> Tabs { get; }
+        private void NewTab()
+        {
+
+        }
+    }
+    
+}
---
+++ Noteorious/NoteTab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noteorious
+{
+    public class NoteTab : Tab
+    {
+        public NoteTab()
+        {
+            Name = "Test";
+        }
+    }
+}
